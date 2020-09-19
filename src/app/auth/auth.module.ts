1c182d1236672e78dad9c55163a8835b355db9b1
chinmay.kalegaonkar <kalegaonkar.chinmay1@gmail.com>
2020-09-19 19:48:05 +0530

    made a login template

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module'
 
@@ -7,6 +9,9 @@ import { AuthRoutingModule } from './auth-routing.module'
 @NgModule({
   declarations: [LoginComponent],
   imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     AuthRoutingModule
   ]
